46100a86623c3d9e91af2969393ef6dd9c8c5ab8
pan3458 <64243958+pan3458@users.noreply.github.com>
2020-05-09 16:09:13 +0800

    添加empinfo接口内容

--- src/main/java/com/gxzy/salary/points/controller/EmpInfoController.java
+++ src/main/java/com/gxzy/salary/points/controller/EmpInfoController.java
@@ -1,8 +1,17 @@
 package com.gxzy.salary.points.controller;
 
 
+import com.gxzy.salary.basic.vo.BasicFilterVo;
+import com.gxzy.salary.core.http.HttpResult;
+import com.gxzy.salary.points.service.EmpInfoService;
+import com.gxzy.salary.util.CommUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,5 +25,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/empInfo")
 public class EmpInfoController {
+    /**
+     * 日志对象
+     */
+    private static final Logger logger = LoggerFactory.getLogger(EmpInfoController.class);
+
+    @Autowired
+    private EmpInfoService empInfoService;
+    
+
+    @GetMapping(value = "/findEmpInfos")
+    public HttpResult findEmpInfos() {
+
+        return HttpResult.ok(empInfoService.findAll());
+    }
+
+    @GetMapping(value = "/findForMonth")
+    public HttpResult findForMonth() {
+        // 取当月积分
+        String monthTime = CommUtils.getMonthFirst();
+        return HttpResult.ok(empInfoService.findForMonth(monthTime));
+    }
+
+    @GetMapping(value = "/findEmpByName")
+    public HttpResult findEmpByName(@RequestParam String name) {
+
+        // 构造查询filter
+
+        BasicFilterVo filter = new BasicFilterVo(name,CommUtils.getMonthFirst());
+        logger.info("findEmpByName**filter**"+filter);
+        return HttpResult.ok(empInfoService.findEmpByName(filter));
+    }
 
 }
--- src/main/java/com/gxzy/salary/points/service/impl/EmpInfoServiceImpl.java
+++ src/main/java/com/gxzy/salary/points/service/impl/EmpInfoServiceImpl.java
@@ -1,9 +1,13 @@
 package com.gxzy.salary.points.service.impl;
 
+import com.gxzy.salary.basic.vo.BasicFilterVo;
+import com.gxzy.salary.core.exception.MyException;
 import com.gxzy.salary.points.model.EmpInfo;
 import com.gxzy.salary.points.dao.EmpInfoMapper;
 import com.gxzy.salary.points.service.EmpInfoService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +22,38 @@ import java.util.List;
  */
 @Service
 public class EmpInfoServiceImpl  implements EmpInfoService {
+
+    /**
+     * 日志对象
+     */
+    private static final Logger logger = LoggerFactory.getLogger(EmpInfoServiceImpl.class);
+
+    @Autowired
+    private EmpInfoMapper empInfoMapper;
+
+    @Override
+    public List<EmpInfo> findAll() {
+        logger.info("*empInfo*findAll*selectAll*");
+        return empInfoMapper.selectAll();
+    }
+
+    @Override
+    public List<EmpInfo> findForMonth(String monthTime) {
+        logger.info("*empInfo**findForMonth*" + monthTime);
+        return empInfoMapper.findForMonth(monthTime);
+    }
+
+    @Override
+    public EmpInfo findEmpByName(BasicFilterVo filter) {
+        try{
+            EmpInfo basicInfo = empInfoMapper.findEmpByName(filter);
+            return basicInfo;
+        }catch (Exception e)
+        {
+            throw new MyException("当月积分未录入，请先录入数据");
+        }
+
+    }
     @Override
     public int save(EmpInfo record) {
         return 0;
